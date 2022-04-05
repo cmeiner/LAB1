d96cfc28ecbbada41ae44e4df393c756d50bd82f
miMeiner <christian.meiner@medieinstitutet.se>
2022-04-05 15:57:08 +0200

    [GET] Hämta info om box, även ID laddas in enkelt Fixes #1

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 const express = require('express')
 const fs = require('fs')
 const port = 3000
-const server = express()
+const app = express()
 
-const boxFile = './data/boxes.json'
+const boxFile = './boxes.json'
 const boxes = require(boxFile)
 
-server.get
+app.get('/boxes', (req, res) => {
+    res.send(boxes)
+});
 
-server.post
+app.get('/boxes/:ID', (req, res) => {
+    let boxID = parseInt(req.params.ID)
+    let foundBox = boxes.find((box) => box.id === boxID )
 
-server.put
+    if (!foundBox) return res.status(404).send('The requested box could not be found!')
+    return res.send(foundBox)
+})
 
-server.delete
+app.post('/boxes', (req, res) => {
+    let newList = boxes
+    let newBox = {"id": boxes.length + 1, "color" : "pink", "size" : "tiny", "contains" : "Nocco"}
+    newList.push(newBox)
+
+    fs.writeFile(boxFile, JSON.stringify(newList), function writeJSON(err) {
+        if (err) return console.error(err)
+        console.log('test')
+    })
+    return res.send('La till ny box ' + JSON.stringify(newBox) )
+})
+
+ 
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`)
+})
+
+app.put('/boxes/:ID', (req, res) => {
+    let boxID = parseInt(req.params.ID)
+    let foundBox = boxes.find((box) => boxes.id === BoxID)
+
+    if(!foundBox) return res.status(404).send('The requested box could not be found!')
+
+    let updatedBox = {"id": boxID, "color" : "idk green or something", "size" : "prob tiny", "contains" : "Nocco"}
+
+
+})
+
+// app.post
+
+// server.delete
