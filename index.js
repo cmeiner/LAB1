b80a7eb4e43a2b072ea0b49e2ccbaeb83d4f321d
miMeiner <christian.meiner@medieinstitutet.se>
2022-04-05 12:08:38 +0200

    Initializing the project

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const fs = require('fs')
+const port = 3000
+const server = express()
+
+const boxFile = './data/boxes.json'
+const boxes = require(boxFile)
+
+server.get
+
+server.post
+
+server.put
+
+server.delete
