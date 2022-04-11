68007b3e7a746a72d40da29288aa449d94a3d6fb
MiMeiner <Christian.Meiner@medieinstitutet.se>
2022-04-11 19:39:07 +0200

    Snabb fix i filen för JSON Fixes #7

--- index.js
+++ index.js
@@ -6,91 +6,70 @@ const boxFile = "./boxes.json";
 const boxes = require(boxFile);
 
 app.use("/", express.static("public"));
+app.use(express.json());
 
-app.get("/boxes", (req, res) => {
-  res.send(boxes);
+app.get("/api/boxes", (req, res) => {
+  res.json(boxes);
 });
 
-app.get("/boxes/:ID", (req, res) => {
+app.get("/api/boxes/:ID", (req, res) => {
   let boxID = parseInt(req.params.ID);
   let foundBox = boxes.find((box) => box.id === boxID);
 
   if (!foundBox)
-    return res.status(404).send("The requested box could not be found!");
-  return res.send(foundBox);
+    return res.status(404).json("The requested box could not be found!");
+  return res.json(foundBox);
 });
 
-app.post("/boxes", (req, res) => {
-  let newList = boxes;
+app.post("/api/boxes", (req, res) => {
   let newBox = {
+    ...req.body,
     id: boxes.length + 1,
-    color: "pink",
-    size: "tiny",
-    contains: "Nocco",
   };
-  newList.push(newBox);
+  boxes.push(newBox);
 
-  fs.writeFile(
-    boxFile,
-    JSON.stringify(newList, null, 2),
-    function writeJSON(err) {
-      if (err) return console.error(err);
-      console.log("test");
-    }
-  );
-  return res.send("La till ny box " + JSON.stringify(newBox));
+  fs.writeFile(boxFile, JSON.stringify(boxes, null, 1), (err) => {
+    if (err) return console.error(err);
+    return res.json(newBox);
+  });
 });
 
-app.put("/boxes/:ID", (req, res) => {
+app.put("/api/boxes/:ID", (req, res) => {
   let boxID = parseInt(req.params.ID);
   let foundBox = boxes.find((box) => box.id === boxID);
 
   if (!foundBox)
-    return res.status(404).send("The requested box could not be found!");
+    return res.status(404).json("The requested box could not be found!");
 
-  let updatedBox = {
-    id: boxID,
-    color: "idk green or something",
-    size: "prob tiny",
-    contains: "Nocco",
-  };
   let updatedBoxes = boxes.map((box) => {
     if (box.id === boxID) {
-      box = updatedBox;
-      return box;
+      return req.body;
     }
     return box;
   });
-  fs.writeFile(
-    boxFile,
-    JSON.stringify(updatedBoxes, null, 2),
-    function writeJSON(err) {
-      if (err) return console.error(err);
-      console.log("Box ändrad");
-    }
-  );
-  return res.send(
-    "Uppdaterade box#" + boxID + " med ändringen: " + JSON.stringify(updatedBox)
-  );
+
+  fs.writeFile(boxFile, JSON.stringify(updatedBoxes, null, 1), (err) => {
+    if (err) return console.error(err);
+    return res.json(req.body);
+  });
 });
 
-app.delete("/boxes/:ID", (req, res) => {
+app.delete("/api/boxes/:ID", (req, res) => {
   let boxID = parseInt(req.params.ID);
   let foundBox = boxes.find((box) => box.id === boxID);
 
   if (!foundBox)
-    return res.status(404).send("The requested box could not be found!");
+    return res.status(404).json("The requested box could not be found!");
 
   let updatedBoxes = boxes.filter((box) => box.id !== boxID);
-  fs.writeFile(
-    boxFile,
-    JSON.stringify(updatedBoxes, null, 2),
-    function writeJSON(err) {
-      if (err) return console.error(err);
-      console.log("Box borttagen");
-    }
-  );
-  return res.send("Box#" + boxID + " borttagen.");
+  fs.writeFile(boxFile, JSON.stringify(updatedBoxes, null, 1), (err) => {
+    if (err) return console.error(err);
+    return res.json("Box#" + boxID + " borttagen.");
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json("Resource not found");
 });
 
 app.listen(port, () => {
--- public/logic.js
+++ public/logic.js
@@ -1,12 +1,12 @@
 window.addEventListener("load", (event) => {
   document
-    .getElementById("button")
+    .getElementById("boxButton")
     .addEventListener("click", (e) => getBoxes());
 });
 
 const getBoxes = async () => {
   try {
-    const res = await fetch("../boxes.json");
+    const res = await fetch("/boxes");
     const result = await res.json();
     renderBox(result);
   } catch (err) {
@@ -14,12 +14,24 @@ const getBoxes = async () => {
   }
 };
 
+// const goToBox = asynd () => {
+//   try {
+//     const res = await fetch('/boxes/:ID')
+//   }
+// }
+
 const renderBox = (boxes) => {
   let boxContainer = document.getElementById("main-content");
   boxContainer.innerHTML = null;
 
   for (let box of boxes) {
-    let boxLayout = `<h1>${box.id}</h1>`;
+    let boxLayout = `<div id="renderedCard"> 
+      <div>BoxID: #${box.id}</div>
+      <div>Color: ${box.color}</div>
+      <div>Contains: ${box.contains}</div>
+      <div>Size: ${box.size}</div>
+      <button id="goToButton">Go to box </button>
+    </div>`;
     boxContainer.innerHTML = boxContainer.innerHTML + boxLayout;
   }
 };
